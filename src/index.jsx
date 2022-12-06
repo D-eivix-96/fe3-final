15b598bd6546b33bdb7b8cf8560f4d5c34519a8d
D-eivix-96 <deivix-oc-0828@hotmail.com>
2022-12-06 05:35:06 -0500

    Creación rutas del proyecto

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './Routes/Home';
+import Contact from './Routes/Contact';
+import Detail from './Routes/Detail';
+import Favs from './Routes/Favs';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <App/>
-  </React.StrictMode>
+  //<React.StrictMode>
+      <BrowserRouter>
+       <Routes>
+          <Route path='/' element={<App />}>
+              <Route path="home" element={<Home />} />
+              <Route path="contacto" element={<Contact />} />
+              <Route path="dentist/:id" element={<Detail />} />
+              <Route path="destacados" element={<Favs />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  //</React.StrictMode>
 );
 
 
